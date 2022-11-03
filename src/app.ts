f7b69e4b238fcb89ff1acc36dd72a498fcf24774
Neto Calegari <netokalegari@gmail.com>
2022-11-03 15:32:09 -0300

    feat: criação de moradores de rua e middleware para nao repetição de nome

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from "express";
 
 import { Request, Response, NextFunction } from "express";
 import { AppError } from "./errors/appError";
+import homelessRoutes from "./Router/homeless.routes";
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use("/teste", (resp, res) => {
   return res.status(201).json({ ok: "tudo ok" });
 });
+
+app.use('/homeless', homelessRoutes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
