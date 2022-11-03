3e35aeae0166dceeb636a8b9455b68f3abeb87e8
emerson gonçalves <emer_gs@hotmail.com>
2022-11-03 10:15:05 -0300

    feat:inclusão da rota institute no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import institutionRoutes from "./Router/institution.routes";
 
 const app = express();
 app.use(express.json());
-
-app.use("/teste", (resp, res) => {
-  return res.status(201).json({ ok: "tudo ok" });
-});
+app.use('/register/institution',institutionRoutes);
 
 export default app;
