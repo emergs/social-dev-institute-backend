b245311680958c06515af73dceaab17e9e603e48
Antonio pedro s <salviano36789@gmail.com>
2022-11-03 11:16:43 -0300

    Feat: Adicionando global error

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,30 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./errors/appError";
+
 const app = express();
 app.use(express.json());
 
 app.use("/teste", (resp, res) => {
   return res.status(201).json({ ok: "tudo ok" });
+});
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
 });
 
 export default app;
