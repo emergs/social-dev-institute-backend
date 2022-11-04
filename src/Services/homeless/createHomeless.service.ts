ceec8c8c29d3aa941d43e5bdbd222088deda8846
Neto Calegari <netokalegari@gmail.com>
2022-11-04 11:35:22 -0300

    feat: relação entre a table homeless e institutions

--- src/Services/homeless/createHomeless.service.ts
+++ src/Services/homeless/createHomeless.service.ts
@@ -1,10 +1,12 @@
 import AppDataSource from "../../data-source";
 import { Homeless } from "../../entities/homeless.entity";
+import { Institutions } from "../../entities/institutions.entity";
 import { AppError } from "../../errors/appError";
 import { IHomelessRequest } from "../../interfaces/homeless";
 
-const createHomelessService = async ({name, age, picture}: IHomelessRequest): Promise<Homeless> => {
+const createHomelessService = async ({name, age, picture, institution}: IHomelessRequest): Promise<Homeless> => {
   const homelessRepository = AppDataSource.getRepository(Homeless);
+  const institutionRepository = AppDataSource.getRepository(Institutions);
 
   // const dataContent  = Object.keys(data);
 
@@ -18,13 +20,21 @@ const createHomelessService = async ({name, age, picture}: IHomelessRequest): Pr
 
   if (!age) {
     throw new AppError(400, 'Age is missing');
-  }; 
+  };
+
+  const institutionObj = await institutionRepository.findOneBy({
+    name: institution
+  });
+
+  if (!institution || !institutionObj) {
+    throw new AppError(400, 'Institution not found');
+  };
 
   const newHomeless = new Homeless();
   newHomeless.name = name;
   newHomeless.age = age;
   newHomeless.picture = picture ? picture : null;
-  // newHomeless.institution = data.institution;
+  newHomeless.institution = institutionObj;
 
   homelessRepository.create(newHomeless);
   await homelessRepository.save(newHomeless);
