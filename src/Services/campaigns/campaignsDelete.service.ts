1f4828791d2c87146ffac67d8116bf5269bacff6
Antonio pedro s <salviano36789@gmail.com>
2022-11-04 20:41:42 -0300

    fix/Campaigns erro de tipo

--- src/Services/campaigns/campaignsDelete.service.ts
+++ src/Services/campaigns/campaignsDelete.service.ts
@@ -1,32 +1,24 @@
-import AppDataSource from "../../data-source"
-import { Campaigns } from "../../entities/campaign.entities"
-import { AppError } from "../../errors/appError"
-    
-    
-    const campaignsDeleteService = async (id: string): Promise<void> => {
-    
-        const campaignsRepository = AppDataSource.getRepository(Campaigns)
-        const findCampaigns = await campaignsRepository.findOneBy({
-            id
-        })
-        
-        if(!findCampaigns){
-            throw new AppError(404, 'User is already inactive')
-            
-        }
-        
-        if(!findCampaigns?.isAlive) {
-            throw new AppError(400, 'User is already inactive')
-           
-        }
-        
-        await campaignsRepository.update(
-            id,
-            {
-                isAlive: false
-            }
-        )    
-        
-    }
-    
-    export default campaignsDeleteService
+import AppDataSource from "../../data-source";
+import { Campaigns } from "../../entities/campaign.entities";
+import { AppError } from "../../errors/appError";
+
+const campaignsDeleteService = async (id: string): Promise<void> => {
+  const campaignsRepository = AppDataSource.getRepository(Campaigns);
+  const findCampaigns = await campaignsRepository.findOneBy({
+    id,
+  });
+
+  if (!findCampaigns) {
+    throw new AppError(404, "User is already inactive");
+  }
+
+  if (!findCampaigns?.isActive) {
+    throw new AppError(400, "User is already inactive");
+  }
+
+  await campaignsRepository.update(id, {
+    isActive: false,
+  });
+};
+
+export default campaignsDeleteService;
--- src/Services/campaigns/campaignsUpdate.service.ts
+++ src/Services/campaigns/campaignsUpdate.service.ts
@@ -1,30 +1,31 @@
-import AppDataSource from "../../data-source"
-import { AppError } from "../../errors/appError"
-import { Campaigns } from "../../entities/campaign.entities"
-import { ICampaignsRequest } from "../../interfaces/campaigns"
+import AppDataSource from "../../data-source";
+import { AppError } from "../../errors/appError";
+import { Campaigns } from "../../entities/campaign.entities";
+import { ICampaignsRequest } from "../../interfaces/campaigns";
 
-const updateCampaignsService = async({ name }: ICampaignsRequest, id: string ): Promise<Campaigns> => {
-    const campaignsRepository = AppDataSource.getRepository(Campaigns)
-    const findCampaigns = await campaignsRepository.findOneBy({
-        id
-    })
+const updateCampaignsService = async (
+  { name }: ICampaignsRequest,
+  id: string
+): Promise<Campaigns> => {
+  const campaignsRepository = AppDataSource.getRepository(Campaigns);
+  const findCampaigns = await campaignsRepository.findOneBy({
+    id,
+  });
 
-    if(!findCampaigns){
-        throw new AppError(404, 'User not found')
-    }
+  if (!findCampaigns) {
+    throw new AppError(404, "User not found");
+  }
 
-    await campaignsRepository.update(
-        id,
-        {
-            id: findCampaigns.id,
-            name:name ? name : findCampaigns.name,
-            isAlive: findCampaigns.isAlive
-        }
-    )    
-        const campaign = await campaignsRepository.findOneBy({  // instituição
-            id
-        })
-    return campaign!
-}
+  await campaignsRepository.update(id, {
+    id: findCampaigns.id,
+    name: name ? name : findCampaigns.name,
+    isActive: findCampaigns.isActive,
+  });
+  const campaign = await campaignsRepository.findOneBy({
+    // instituição
+    id,
+  });
+  return campaign!;
+};
 
-export default updateCampaignsService
+export default updateCampaignsService;
