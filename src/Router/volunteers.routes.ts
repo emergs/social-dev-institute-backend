89ea7444a60e7f8c8ff54f4cefbefc18f0d3f5ec
clayson <clayson420eufrasio@gmail.com>
2022-11-05 08:45:12 -0300

    feat:Cração rota de login do voluntario / criado verifyTokenVoluntary

--- src/Router/volunteers.routes.ts
+++ src/Router/volunteers.routes.ts
@@ -1,22 +1,30 @@
 import { Router } from "express";
-import { volunteersCreateController, volunteersListController, volunteersUpdateController } from "../Controllers/volunteers.controller";
+import { volunteersCreateController, volunteersListController, volunteersLoginController, volunteersUpdateController } from "../Controllers/volunteers.controller";
+import verifyTokenVoluntaryMiddleware from "../Middlewares/verifyTokenVoluntary.middleware";
 import verifyVoluntaryExists from "../Middlewares/verifyVoluntaryExists";
 
 const volunteersRoutes = Router()
 
 volunteersRoutes.post(
-  "/voluntary",
+  "/register",
   verifyVoluntaryExists,
   volunteersCreateController
 )
 
+volunteersRoutes.post(
+  "/login",
+  volunteersLoginController
+)
+
 volunteersRoutes.get(
   "",
+  verifyTokenVoluntaryMiddleware,
   volunteersListController
 )
 
 volunteersRoutes.patch(
   "/:id",
+  verifyTokenVoluntaryMiddleware,
   volunteersUpdateController
 )
 
