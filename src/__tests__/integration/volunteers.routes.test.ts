91224456b09fac1d327b105aec137ec593692710
clayson <clayson420eufrasio@gmail.com>
2022-11-05 09:59:45 -0300

    feat: configurado testes de login dos voluntarios / teste da atualização do voluntario com e sem autenticação / teste da listagem de todos os voluntarios com e sem autorização

--- src/__tests__/integration/volunteers.routes.test.ts
+++ src/__tests__/integration/volunteers.routes.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../../app";
 import { DataSource } from "typeorm";
 import AppDataSource from "../../data-source";
-import { volunteerRequest, volunteerUpdate } from "../mocks/volunteers.mocks.";
+import { volunteerLogin, volunteerRequest } from "../mocks/volunteers.mocks.";
 
 describe("Cadastra um voluntário", () => {
 
@@ -22,9 +22,9 @@ describe("Cadastra um voluntário", () => {
     await connection.destroy()
   })
 
-  test("POST /register/voluntary -> Deve permitir o cadastro de um voluntário", async () => {
+  test("POST /volunteers/register -> Deve permitir o cadastro de um voluntário", async () => {
 
-    const resultVoluntary = await request(app).post("/register/voluntary").send(volunteerRequest)
+    const resultVoluntary = await request(app).post("/volunteers/register").send(volunteerRequest)
 
     expect(resultVoluntary.status).toBe(201)
     expect(resultVoluntary.body).toHaveProperty("id")
@@ -37,19 +37,69 @@ describe("Cadastra um voluntário", () => {
 
   })
 
+  test("POST /volunteers/login -> Deve ser posivel logar um voluntario", async () => {
+    const response = await request(app).post("/volunteers/login").send(volunteerLogin);
 
-  test("PATCH /voluntary/:id -> Deve permitir a atualização de um voluntário", async () => {
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("token")
 
-    const resultVoluntary = await request(app).patch("/voluntary/:id").send(volunteerUpdate)
+  })
 
-    expect(resultVoluntary.status).toBe(201)
-    expect(resultVoluntary.body).toHaveProperty("id")
-    expect(resultVoluntary.body).toHaveProperty("cpf")
-    expect(resultVoluntary.body).toHaveProperty("age")
-    expect(resultVoluntary.body).toHaveProperty("name")
-    expect(resultVoluntary.body).toHaveProperty("email")
-    expect(resultVoluntary.body).toHaveProperty("telephone")
-    expect(resultVoluntary.body).not.toHaveProperty("password")
+  test("POST /volunteers/login -> Não deve ser possivel logar com password ou email incorreto", async () => {
+    const response = await request(app).post("/volunteers/login").send({
+      email: "asdasdffssafd@mail.com",
+      password: "asddasjkdjhsad"
+    });
+
+    expect(response.body).toHaveProperty("message")
+    expect(response.status).toBe(403)
+
+  })
+
+  test("GET /volunteers -> Não deve listar os voluntarios sem autenticação", async () => {
+    const response = await request(app).get('/volunteers')
+
+    expect(response.body).toHaveProperty("message")
+    expect(response.status).toBe(401)
+
+  })
+
+  test("GET /volunteers -> Deve listar todos voluntarios", async () => {
+    await request(app).post('/volunteers').send(volunteerRequest)
+    const voluntaryLoginResponse = await request(app).post("/login").send(volunteerLogin);
+    const response = await request(app).get('/volunteers').set("Authorization", `Bearer ${voluntaryLoginResponse.body.token}`)
+
+    expect(response.body)
+
+  })
+
+  test("PATCH /volunteers/:id -> Não deve ser possivel atualizar um voluntario sem autorização", async () => {
+    const voluntaryLoginResponse = await request(app).post("/volunteers/login").send(volunteerLogin);
+    const voluntaryTobeUpdate = await request(app).get('/volunteers').set("Authorization", `Bearer ${voluntaryLoginResponse.body.token}`)
+    const response = await request(app).patch(`/volunteers/${voluntaryTobeUpdate.body[0].id}`)
+
+    expect(response.body).toHaveProperty("message")
+    expect(response.status).toBe(401)
+
+  })
+
+  test("PATCH /volunteers/:id -> Deve permitir a atualização de um voluntário", async () => {
+    const voluntaryLoginResponse = await request(app).post("/volunteers/login").send(volunteerLogin)
+    const token = `Bearer ${voluntaryLoginResponse.body.token}`
+
+    const voluntaryTobeUpdate = await request(app).get('/volunteers').set("Authorization", token)
+    const voluntaryTobeUpdateId = voluntaryTobeUpdate.body[0].id
+
+    const response = await request(app).patch(`/volunteers/${voluntaryTobeUpdateId}`).set("Authorization", token).send(volunteerLogin)
+
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty("id")
+    expect(response.body).toHaveProperty("cpf")
+    expect(response.body).toHaveProperty("age")
+    expect(response.body).toHaveProperty("name")
+    expect(response.body).toHaveProperty("email")
+    expect(response.body).toHaveProperty("telephone")
+    expect(response.body).not.toHaveProperty("password")
 
   })
 })
