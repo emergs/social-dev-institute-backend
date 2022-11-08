95cd5a3447d547eddf34c8473c3d28dad3b64c8d
patrickcordeiro <patrick.cordeiro30@gmail.com>
2022-11-08 14:15:03 -0300

    feat: Criação de testes rota login

--- src/__tests__/mocks/institutions.mocks.ts
+++ src/__tests__/mocks/institutions.mocks.ts
@@ -1,19 +1,21 @@
 import { IInstitutions } from "../../interfaces/institutions";
 
-export const mockedInstitution : IInstitutions = {
+export const mockedInstitution: IInstitutions = {
   name: "Billy",
   email: "billy@mail.com",
-  cnpj:"12345678912345",
-  address:"Alameda Sagrado Coração",
-  phone:"4133224455",
-  password: "123456"
-}
+  cnpj: "12345678912345",
+  address: "Alameda Sagrado Coração",
+  isActive: true,
+  phone: "4133224455",
+  password: "123456",
+};
 
-export const mockedInstitutionInvalidCNPJ : IInstitutions = {
+export const mockedInstitutionInvalidCNPJ: IInstitutions = {
   name: "Billy",
   email: "billy@mail.com",
-  cnpj:"123456789123456789",
-  address:"Alameda Sagrado Coração",
-  phone:"4133224455",
-  password: "123456"
-}
+  cnpj: "123456789123456789",
+  address: "Alameda Sagrado Coração",
+  isActive: true,
+  phone: "4133224455",
+  password: "123456",
+};
