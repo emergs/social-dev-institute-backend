291aabeb7ffd48220b192b989ba0a95897a35d36
Antonio pedro s <salviano36789@gmail.com>
2022-11-08 09:08:08 -0300

    fix/corrigindo erros no migrations e erros no mocks e por ultimo adicionando novos erros na compaigns

--- src/Router/campaigns.routes.ts
+++ src/Router/campaigns.routes.ts
@@ -6,13 +6,30 @@ import {
   campaignsListServiceByIdController,
   updateCampaignsController,
 } from "../Controllers/campaigns.controller";
+import verifyInstitutionExistMiddleware from "../Middlewares/verifyInstitutionExist.middleware";
+import verifyTokenVoluntaryMiddleware from "../Middlewares/verifyTokenVoluntary.middleware";
 
 const campaignsRoutes = Router();
 
-campaignsRoutes.post("", campaignsCreateController);
+campaignsRoutes.post(
+  "",
+  verifyTokenVoluntaryMiddleware,
+  verifyInstitutionExistMiddleware,
+  campaignsCreateController
+);
 campaignsRoutes.get("", campaignsListController);
 campaignsRoutes.get("/:id", campaignsListServiceByIdController);
-campaignsRoutes.delete("/:id", campaignDeleteController);
-campaignsRoutes.patch("/:id", updateCampaignsController);
+campaignsRoutes.delete(
+  "/:id",
+  verifyTokenVoluntaryMiddleware,
+  verifyInstitutionExistMiddleware,
+  campaignDeleteController
+);
+campaignsRoutes.patch(
+  "/:id",
+  verifyTokenVoluntaryMiddleware,
+  verifyInstitutionExistMiddleware,
+  updateCampaignsController
+);
 
 export default campaignsRoutes;
--- src/__tests__/mocks/campaigns.mocks.ts
+++ src/__tests__/mocks/campaigns.mocks.ts
@@ -1,46 +1,55 @@
 import { ICampaigns } from "../../interfaces/campaigns";
 
 export const mockedCampaigns: ICampaigns = {
-    name: "Ana",
-    isAlive: true,
-    address: [{
-        road: "Rua Paraná",
-        number: "123",
-        complement: "ao lado do supermercado",
-        city: "Curitiba",
-        state: "PR"
-        
-    }],
-    instituitionId: ""
-}
+  id: "ef0463e8-62b7-4943-b0c0-4220267dfb9e",
+  name: "Ana",
+  isAlive: true,
+  address: [
+    {
+      road: "Rua Paraná",
+      number: "123",
+      complement: "ao lado do supermercado",
+      city: "Curitiba",
+      state: "PR",
+    },
+  ],
+  date_creation: "",
+  date_update: "",
+  institutionId: "",
+};
 
-export const mockedCampaignInvalidInstitutionId : ICampaigns = {
-    name: "Ana",
-    isAlive: true,
-    address: [{
-        road: "Rua Paraná",
-        number: "123",
-        complement: "ao lado do supermercado",
-        city: "Curitiba",
-        state: "PR"
-        
-    }],
-    instituitionId: "8f9ae6ce-e36c-4d9d-9bd7-b4c98cb4e4f4"
-
-}
+export const mockedCampaignInvalidInstitutionId: ICampaigns = {
+  id: "ef0463e8-62b7-4943-b0c0-4220267dfb9e",
+  name: "Ana",
+  isAlive: true,
+  address: [
+    {
+      road: "Rua Paraná",
+      number: "123",
+      complement: "ao lado do supermercado",
+      city: "Curitiba",
+      state: "PR",
+    },
+  ],
+  date_creation: "",
+  date_update: "",
+  institutionId: "8f9ae6ce-e36c-4d9d-9bd7-b4c98cb4e4f4",
+};
 
 export const mockedCampaignsInvalidStat: ICampaigns = {
-    name: "Ana",
-    isAlive: true,
-    address: [{
-        road: "Rua Paraná",
-        number: "123",
-        complement: "ao lado do supermercado",
-        city: "Curitiba",
-        state: "PRGO"
-        
-    }],
-    instituitionId: ""
-}
-
-
+  id: "ef0463e8-62b7-4943-b0c0-4220267dfb9e",
+  name: "Ana",
+  isAlive: true,
+  address: [
+    {
+      road: "Rua Paraná",
+      number: "123",
+      complement: "ao lado do supermercado",
+      city: "Curitiba",
+      state: "PRGO",
+    },
+  ],
+  date_creation: "",
+  date_update: "",
+  institutionId: "",
+};
