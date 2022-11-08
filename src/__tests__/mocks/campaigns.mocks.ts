6cfd967b53f95f5188c1acf794db39482779d901
Kátya <katyakeila@gmail.com>
2022-11-08 07:27:58 -0300

    fix: atualizando testes da rota campaign

--- src/__tests__/mocks/campaigns.mocks.ts
+++ src/__tests__/mocks/campaigns.mocks.ts
@@ -3,44 +3,44 @@ import { ICampaigns } from "../../interfaces/campaigns";
 export const mockedCampaigns: ICampaigns = {
     name: "Ana",
     isAlive: true,
-    instituitionId: "",
     address: [{
         road: "Rua Paraná",
         number: "123",
         complement: "ao lado do supermercado",
         city: "Curitiba",
         state: "PR"
-
-    }]
+        
+    }],
+    instituitionId: ""
 }
 
 export const mockedCampaignInvalidInstitutionId : ICampaigns = {
     name: "Ana",
     isAlive: true,
-    instituitionId: "8f9ae6ce-e36c-4d9d-9bd7-b4c98cb4e4f4",
     address: [{
         road: "Rua Paraná",
         number: "123",
         complement: "ao lado do supermercado",
         city: "Curitiba",
         state: "PR"
-
-    }]
+        
+    }],
+    instituitionId: "8f9ae6ce-e36c-4d9d-9bd7-b4c98cb4e4f4"
 
 }
 
 export const mockedCampaignsInvalidStat: ICampaigns = {
     name: "Ana",
     isAlive: true,
-    instituitionId: "",
     address: [{
         road: "Rua Paraná",
         number: "123",
         complement: "ao lado do supermercado",
         city: "Curitiba",
         state: "PRGO"
-
-    }]
+        
+    }],
+    instituitionId: ""
 }
 
 
--- src/__tests__/mocks/login.mocks.ts
+++ src/__tests__/mocks/login.mocks.ts
@@ -1,6 +1,13 @@
 import { IUserLogin } from "../../interfaces/users";
+import { IVolunteerLogin } from "../../interfaces/volunteers"
 
 export const mockedInstitutionLogin : IUserLogin = {
     email: "billy@mail.com",
     password: "123456"
+}
+
+export const mockedVolunteersLogin : IVolunteerLogin = {
+    email: "clayson@gmail.com",
+    password: "1234"
+
 }
