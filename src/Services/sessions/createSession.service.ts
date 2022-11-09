003a8be8cd828ad2c6a141165fa5bd52c09c4a68
patrickcordeiro <patrick.cordeiro30@gmail.com>
2022-11-09 10:40:13 -0300

    fix: Corrigindo service de login, email e senha devem ser obrigatórios

--- src/Services/sessions/createSession.service.ts
+++ src/Services/sessions/createSession.service.ts
@@ -8,6 +8,10 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 const createSessionService = async ({ email, password }: IUserLogin) => {
+  if (!email || !password) {
+    throw new AppError(403, "Email and password are required!");
+  }
+
   const volunteerRepository = AppDataSource.getRepository(Volunteers);
   const intitutionRepository = AppDataSource.getRepository(Institutions);
 
